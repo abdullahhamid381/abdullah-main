a5ce43c127a674a603d14e359281d329c636c28c
Abdullah Hamid <hafizabdullahqurashi1@gmail.com>
2024-05-17 23:36:45 +0500

    upadte

--- src/Components/Data.jsx
+++ src/Components/Data.jsx
@@ -92,27 +92,27 @@ export const projects = [
     img: "./images/food.png",
     link:"https://homez-appdir.vercel.app/",
     h1:'Grocerie MarketPlace',
-    para:'The project features a clean and responsive design, user-friendly navigation,I have created a roceries marketplace web app in this user can easily purchase all the groceries item,I created this web app for my client '
+    para:'The project features a clean and responsive design, user-friendly navigation, I have created a roceries marketplace web app in this user can easily purchase all the groceries item,I created this web app for my client '
   },
   {
     img: "./images/resturant.png",
     h1:'Cafeu Resturant',
-    para:'The project features a clean and responsive design, user-friendly navigation,I have created a resturant web app and add all the features related to resturant like that menu, dishes, services, pricing booking etc '
+    para:'The project features a clean and responsive design, user-friendly navigation, I have created a resturant web app and add all the features related to resturant like that menu, dishes, services, pricing booking etc '
   },
   {
     img: "./images/appartment.png",
     h1:'Real State',
-    para:'The project features a clean and responsive design, user-friendly navigation,I have created real state web app for my client In it, user can display the property qualities with images, and then  sell, rent and buy  '
+    para:'The project features a clean and responsive design, user-friendly navigation, I have created real state web app for my client In it, user can display the property qualities with images, and then  sell, rent and buy  '
   },
   {
     img: "./images/tour.png",
     h1:'Tour Agency',
-    para:'The project features a clean and responsive design, user-friendly navigation,I have created a tourism agency web app in which user can easily set the planned tour with popular places with plan packages.'
+    para:'The project features a clean and responsive design, user-friendly navigation, I have created a tourism agency web app in which user can easily set the planned tour with popular places with plan packages.'
   },
   {
     img: "./images/dashboard.png",
     h1:'Admin Dashboard',
-    para:'The project features a clean and responsive design, user-friendly navigation,I have created task monetring admin dashboard in this all the detail avaible according to website like that sales, appointment, members etc '
+    para:'The project features a clean and responsive design, user-friendly navigation, I have created task monetring admin dashboard in this all the detail avaible according to website like that sales, appointment, members etc '
   },
   {
     img: "./images/cloth.png",
@@ -122,14 +122,16 @@ export const projects = [
   {
     img:"./images/doctor.png",
     h1:"Doctor Appointment",
-    para:'The project features a clean and responsive design, user-friendly navigation,I have created a web app for doctors in this web all the detail added like that  shops, medicine, booking appointment, and I created multiple designs in this web app.'
+    para:'The project features a clean and responsive design, user-friendly navigation, I have created a web app for doctors in this web all the detail added like that  shops, medicine, booking appointment, and I created multiple designs in this web app.'
   },
   {
     img:"./images/car.png",
     h1:"Car Showroom",
-    para:'The project features a clean and responsive design, user-friendly navigation,I have created a car showroom website in which user can search for cars according to status and user can easily filter the cars ( new, used, auction) select and buy the car.'
+    para:'The project features a clean and responsive design, user-friendly navigation, I have created a car showroom website in which user can search for cars according to status and user can easily filter the cars ( new, used, auction) select and buy the car.'
   },
   {
     img: "./images/ecom.png",
+    h1:'Cloth Store',
+    para:'The project features a clean and responsive design, user-friendly navigation,I have created a another cloth web store app for my client in this app user can esily buy the clothes, I have added all the features that a shopping store has in it'
   },
 ];
